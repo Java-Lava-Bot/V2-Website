9b177fb5e042b65255067527e9229cc7127125d1
PhillsPhanbh3 <bkohut131@gmail.com>
2025-11-22 17:32:41 -0500

    trying a fix

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -57,5 +57,5 @@ export default function AboutUs() {
       </main>
       <Footer />
     </>
-  );
+);
 }
--- src/app/components/TestimonialsSection.jsx
+++ src/app/components/TestimonialsSection.jsx
@@ -231,7 +231,7 @@ export default function TestimonialsSection() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M15 19l-7-7 7-7"
+                  d="M9 5l7 7-7 7"
                 />
               </svg>
             </button>
