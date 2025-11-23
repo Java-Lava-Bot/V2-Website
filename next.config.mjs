7d8cd28b55420f26e9aefa378a7c99781db66617
PhillsPhanbh3 <bkohut131@gmail.com>
2025-11-22 19:53:02 -0500

    trying a fix suggested by zig and discloud support

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Produce a minimal, self-contained server build for deployment
-  output: 'standalone',
   reactStrictMode: true,
 };
 
